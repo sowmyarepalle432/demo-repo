b532347a1023ca4361e03a7c2c4cb8a9586f7099
sowmyarepalle432 <sowmya.repalle432@gmail.com>
2025-09-10 22:32:58 +0530

    DataTypes

---
+++ Sep10.java
@@ -0,0 +1,18 @@
+class Sep10{
+    public static void main(String[] args){
+        byte value = 127;
+        short value2 = 32767;
+        int value3 = 2147483647;
+        long value4 = 9111111111111111111L;
+        float value5 = 1.5f;
+        double value6 = 234567890.00000000;
+        String data = "java";
+        System.out.println(value);
+        System.out.println(value2);
+        System.out.println(value3);
+        System.out.println(value4);
+        System.out.println(value5);
+        System.out.println(value6);
+        System.out.println(data);
+    }
+}
